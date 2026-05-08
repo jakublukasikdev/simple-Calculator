143c1792cdfbf87f2de84f90ffe2f6da09afd281
Jakub Lukasik <jakublukasik71@gmail.com>
2026-05-08 16:45:01 +0200

    poprawki w klasie Operation

--- src/Operation.java
+++ src/Operation.java
@@ -1,15 +1,20 @@
 public class Operation {
 
     private double result;
-    public int add(){return 0;}
-    public int subtract(){return 0;}
-    public int multiply(){return 0;}
-    public int divide(){return 0;}
-    public void show(){}
+
+    public void add(double x, double y){}
+
+    public void subtract(double x,double y){}
+    public void multiply(double x, double y){}
+    public void divide(double x,double y){}
+    public void show(){
+        System.out.println("wynik dzialania = "+result);
+    }
     public void set(double r){
         this.result=r;
     }
     public double get(){
         return result;
     }
+
 }
