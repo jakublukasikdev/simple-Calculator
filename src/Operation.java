1a82ae03491648c40bc01a0861221313a9c9b263
Jakub Lukasik <jakublukasik71@gmail.com>
2026-05-08 16:09:01 +0200

    implementacja klasy Operation

--- src/Operation.java
+++ src/Operation.java
@@ -1,2 +1,15 @@
 public class Operation {
+
+    private double result;
+    public int add(){return 0;}
+    public int subtract(){return 0;}
+    public int multiply(){return 0;}
+    public int divide(){return 0;}
+    public void show(){}
+    public void set(double r){
+        this.result=r;
+    }
+    public double get(){
+        return result;
+    }
 }
