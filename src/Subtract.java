64ef1fa29b781b5a5a878a2558dfa5a7adb905fc
Jakub Lukasik <jakublukasik71@gmail.com>
2026-05-08 16:37:58 +0200

    implementacja klasy Subtract

--- src/Subtract.java
+++ src/Subtract.java
@@ -1,2 +1,12 @@
-public class Subtract {
+public class Subtract extends Operation {
+    @Override
+    public void  subtract(double x, double y){
+        double s=x-y;
+        super.set(s);
+    }
+
+    @Override
+    public void show(){
+        System.out.println("wynik odejmowania = "+super.get());
+    }
 }
