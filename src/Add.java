1d3b6de1399e23933060406275ce711689052d2d
Jakub Lukasik <jakublukasik71@gmail.com>
2026-05-08 16:29:51 +0200

    implementacja klasy Add

--- src/Add.java
+++ src/Add.java
@@ -1,2 +1,12 @@
-public class Add {
+public class Add extends Operation{
+    @Override
+    public void  add(double x, double y){
+        double s=x+y;
+        super.set(s);
+    }
+
+    @Override
+    public void show(){
+        System.out.println("wynik dodawania = "+super.get());
+    }
 }
